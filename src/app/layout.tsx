828bbb04f7ce5df151ad7cbd4f94e1cdca000c59
Dhika Rizky <deka_r@outlook.com>
2024-03-06 16:51:53 +0700

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import { ReactElement } from 'react'
+// eslint-disable-next-line camelcase
+import { Plus_Jakarta_Sans } from 'next/font/google'
+import '../styles/global.css'
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  weight: 'variable',
+  display: 'swap',
+  fallback: ['system-ui', 'arial'],
+  subsets: ['latin']
+})
+
+export default function Layout ({ children }: { children: ReactElement }) {
+  return (
+    <html lang="en">
+    <head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Touching Grass</title>
+    </head>
+    <body className={`${plusJakartaSans.className} font-medium bg-[#E6F1E1] text-[#5B8F21] grid md:grid-cols-[320px_minmax(0,_1fr)] md:py-10 md:px-12 gap-4`}>
+      <div className="sticky top-0 mt-12 md:mt-0 z-10">
+        <div className="px-8 pt-8 pb-4 md:px-0 md:pt-0 md:pb-0 bg-[#E6F1E1]">
+          <span className="lowercase text-2xl md:text-[64px] font-extrabold leading-none">Touching<br />Grass</span>
+        </div>
+        <div className="bg-gradient-to-b from-[#E6F1E1] to-transparent h-1 w-full md:hidden" />
+      </div>
+      <div className="pb-8 px-8 md:pb-0 md:px-0">
+        { children }
+      </div>
+    </body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,43 @@
+import { ImageField } from '@prismicio/client'
+import { createClient } from '../prismicio'
+import { PrismicNextImage } from '@prismicio/next'
+
+export default async function Index () {
+  const client = createClient()
+
+  const images = await client.getAllByType('image')
+
+  const getCheckedAspectRatioClass = (image: ImageField) => {
+    switch ((image.dimensions?.width ?? 0) / (image.dimensions?.height ?? 0)) {
+      case 4 / 3: return 'peer-checked:aspect-[4/3]'
+      case 3 / 4: return 'peer-checked:aspect-[3/4]'
+      case 9 / 16: return 'peer-checked:aspect-[9/16]'
+      default: return 'peer-checked:aspect-video'
+    }
+  }
+
+  return (
+    <main className="grid md:grid-cols-3 gap-4">
+      {images.map(image => (
+        <label htmlFor={image.uid} key={image.uid} className="relative rounded-lg overflow-hidden bg-[#c3dda4] group">
+          <input type="checkbox" id={image.uid} className="peer hidden" />
+          <span className="absolute bottom-0 left-0 font-bold py-2 px-4 truncate w-[90%]">{image.data.title}</span>
+          <div className="fixed left-0 top-0 bg-black w-screen h-screen opacity-0 transition-opacity peer-checked:opacity-40 pointer-events-none peer-checked:pointer-events-auto z-20" />
+          <PrismicNextImage
+            alt=""
+            field={image.data.image}
+            imgixParams={{ q: 75, cs: 'srgb', ar: '4:3', fit: 'crop', crop: ['focalpoint', 'entropy'] }}
+            className="transition-y-0 relative w-full h-auto object-cover aspect-[4/3] rounded-lg overflow-hidden cursor-pointer group-hover:-translate-y-10 group-hover:drop-shadow-2xl opacity-100 transition-[filter,_transform,_opacity] peer-checked:opacity-0 peer-checked:translate-y-full duration-200"
+          />
+
+          <PrismicNextImage
+            alt=""
+            field={image.data.image}
+            imgixParams={{ q: 75, cs: 'srgb', ar: `${image.data.image.dimensions?.width ?? 0}:${image.data.image.dimensions?.height ?? 0}`, fit: 'crop' }}
+            className={`aspect-[4/3] fixed w-full h-auto object-cover rounded-lg cursor-pointer z-20 max-w-6xl opacity-0 transition-[left,_top,_position,_aspect-ratio,_transform,_opacity] left-[50vw] top-[50vh] translate-y-[0%] translate-x-[-50%] pointer-events-none peer-checked:opacity-100 ${getCheckedAspectRatioClass(image.data.image)} peer-checked:translate-y-[-50%] peer-checked:drop-shadow-xl p-4 md:p-8 peer-checked:pointer-events-auto`}
+          />
+        </label>
+      ))}
+    </main>
+  )
+}
